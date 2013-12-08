cc89a2ea41f96dba82fb9d3d2c9c0323fee06e29
Mandy <mcswinton@gmail.com>
2013-12-08 14:05:55 -0500

    added overlap function

--- js/itinerary.js
+++ js/itinerary.js
@@ -486,6 +486,8 @@ function addSingleVenue() {
 			return -1;
 		return 0;
 	});
+
+	
 	
 	// clear out table
 	$("#venue-table-tbody").html(" ");
@@ -500,6 +502,33 @@ function addSingleVenue() {
 		
 		displayVenue(venue);
 	});
+	detectCollision();
+}
+
+function detectCollision(){
+	for (var i = 1; i< itinerary.itinerary.length-1; i++){
+		var beforeStart = (itinerary.itinerary[i-1].startDate);
+		var beforeEnd = (itinerary.itinerary[i-1].endDate);
+		var currentStart = (itinerary.itinerary[i].startDate);
+		var currentEnd = (itinerary.itinerary[i].endDate);
+		var afterStart = (itinerary.itinerary[i+1].startDate);
+		var afterEnd = (itinerary.itinerary[i+1].endDate);
+		console.log("ID " + itinerary.itinerary[i].id);
+
+		if (currentStart < beforeEnd){
+			console.log("COLLISIONTop");
+			//grab tr- id. 
+			//$("#tr-" +(itinerary.itinerary[i].id)).html("Well crap");
+			//alert($("#tr-" +(itinerary.itinerary[i].id)).html());
+			$("#tr-" +(itinerary.itinerary[i].id)).css("border-top","5px solid rgba(255, 0, 0, .3)");
+			//$("#tr-" +(itinerary.itinerary[i].id)).css("border-top","4px solid red");
+		} 
+		if (currentEnd > afterStart){
+			console.log("COLLISIONBottom");
+				$("#tr-" +(itinerary.itinerary[i].id)).css("border-bottom","1px solid rgba(255, 0, 0, .5)");
+		}
+
+	}
 }
 
 // Add venue to itinerary when add button clicked 
